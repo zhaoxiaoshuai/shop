7185744308f4ea8216806c23e7068c0bf7e8e998
15617859237 <15617859237@163.com>
2017-07-07 21:53:36 +0800

    hy test

---
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Orders;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Http\Model\Orders;
+use App\Http\Model\Detail;
+use App\Http\Model\User;
+
+class OrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 订单管理
+     * author:杨滨瑞
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {   
+        
+
+        //通过user_id查询user_name
+        //带搜索条件分页查询
+        if($request->has('orders')){
+            //查询的订单号
+            $orders = trim($request->input('orders'));
+
+            $data = Orders::join('user','user.user_id','=','orders.user_id')->where('order_id',$orders)->paginate(5);
+            
+            return view('admin.orders.index',['data'=>$data,'orders'=>$orders]);
+        }else{
+            $data = Orders::join('user','user.user_id','=','orders.user_id')->orderBy('id','desc')->paginate(5);
+            // dd($data);
+            
+            return view('admin.orders.index',['data'=>$data]);
+        };
+        
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        //订单详情列表
+        $data = Detail::join('orders','detail.order_id','=','orders.order_id')->join('goods','goods.good_id','=','detail.good_id')->get();
+        dd($data);
+        // return view('admin.orders')
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($orders)
+    {
+        //找到要修改的订单,显示到页面
+        $data = Orders::join('user','user.user_id','=','orders.user_id')->where('order_id',$orders)->first();
+        // dd(date('Y-m-d H:i:s',$data['order_time']));
+        
+        return view('admin.orders.edit',['data'=>$data]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $orders)
+    {
+        // dd($request->all());
+        // dd($orders);
+        $input =  $request->except(['_token',"_method"]);
+        // dd($input);
+        $res = Orders::where('order_id',$orders)->update($input);
+        if($res){
+//            如果成功，返回到订单列表页
+            return redirect('admin/orders');
+        }else{
+//            如果失败，返回去
+            return back()->with('error',"修改失败");
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($order)
+    {
+        //删除对应的订单号
+       $re =  Orders::where('order_id',$order)->delete();
+//       0表示成功 其他表示失败
+       if($re){
+           $data = [
+                'status'=>0,
+                'msg'=>'删除成功！'
+           ];
+       }else{
+           $data = [
+               'status'=>1,
+               'msg'=>'删除失败！'
+           ];
+       }
+       return $data;
+    }
+}
