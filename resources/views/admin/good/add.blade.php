379ae54ce79010fcb67addd05987d90b5a677f28
guochengjie31 <m18640976528@163.com>
2017-07-07 20:16:04 +0800

    test

--- resources/views/admin/good/add.blade.php
+++ resources/views/admin/good/add.blade.php
@@ -94,7 +94,7 @@
                                             success: function(data) {
 //                                    console.log(data);
 //                                    alert("上传成功");
-                                                $('#pic').attr('src','/'+data);
+                                                $('#pic').attr('src','http://gcj182.oss-cn-qingdao.aliyuncs.com/'+data);
                                                 $('#pic').show();
                                                 $('#good_pic').val(data);
 
